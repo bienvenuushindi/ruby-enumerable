f0e70fe7f67a1e3a7e4fc113dd8b94f5a06dd8ee
kadenyi <treykadenyi@gmail.com>
2022-09-23 11:16:39 +0300

    Replace the while loops with the each method from the class

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -2,11 +2,8 @@ module MyEnumerable
   def all?
     return 'No block given' unless block_given?
 
-    i = 0
-    while i < @list.length
-      return false unless yield(@list[i])
-
-      i += 1
+    each do |e|
+      return false unless yield(e)
     end
     true
   end
@@ -14,22 +11,16 @@ module MyEnumerable
   def any?
     return 'No block given' unless block_given?
 
-    i = 0
-    while i < @list.length
-      return true if yield(@list[i])
-
-      i += 1
+   each do |e|
+      return true if yield(e)
     end
     false
   end
 
   def filter(&block)
     result = []
-    i = 0
-    while i < @list.length
-      result << @list[i] if block.call(@list[i])
-
-      i += 1
+    each do |e|
+      result << e if block.call(e)
     end
     result
   end

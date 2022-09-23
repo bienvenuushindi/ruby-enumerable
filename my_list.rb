3d0f381790dc6d0cfd27a73f24ba6ec3150a538b
kadenyi <treykadenyi@gmail.com>
2022-09-23 11:16:10 +0300

    Modify the 'each' function

--- my_list.rb
+++ my_list.rb
@@ -2,11 +2,11 @@ require_relative 'my_enumerable'
 class MyList
   # include MyEnumerable
   include MyEnumerable
-  def initialize(list)
+  def initialize(*list)
     @list = list
   end
 
-  def each(&block)
-    @list.each(block)
+ def each
+   @list.each { |e| yield(e) }
   end
 end
